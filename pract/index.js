0a492067730b53d346d6fa2ffc6a9c8ffea60f63
Dmytro <Dmytro>
2021-01-16 17:28:19 +0200

    first point

---
+++ pract/index.js
@@ -0,0 +1,21 @@
+// const numberOfFilms = prompt('Скільки фільмів ви переглянули?');
+
+// let personalMovieDB = {
+//   count : numberOfFilms,
+//   movies : {},
+//   actors : {},
+//   geners : [],
+//   privat : false
+// }
+
+// console.log(personalMovieDB);
+
+const firstQuest = prompt('Один із останніх фільмів, який ви дивились?');
+const secondQuest = prompt('На скільки ви оціните цей фільм?');
+
+let movies = {
+}
+
+movies.filmsName = firstQuest;
+movies.filmsGrad = secondQuest;
+ console.log(movies);
