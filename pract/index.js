3338f81ee82b52d56e13f458d805f61a994f1f36
Dmytro <Dmytro>
2021-01-18 00:22:13 +0200

    second prac done

--- pract/index.js
+++ pract/index.js
@@ -10,11 +10,28 @@ let personalMovieDB = {
 
 for(let i = 0; i < 2; i++){
 let firstQuest = prompt('Один із останніх фільмів, який ви дивились?');
-let secondQuest = prompt('На скільки ви оціните цей фільм?');
+
+if(firstQuest === " " || firstQuest === "" || firstQuest.length > 50 || firstQuest == null){
+  firstQuest = prompt('Введіть один із переглянутих фільмів коректно!')
+} 
+
+let secondQuest = prompt('На скільки ви оціните цей фільм від 0 до 10?');
+
+if(isNaN(secondQuest) || secondQuest == null){
+  secondQuest = prompt('Введіть коректне значення оцінки фільму в числовому вигляді!');
+}
 
 personalMovieDB.movies[firstQuest] = secondQuest;
-personalMovieDB.movies[firstQuest] = secondQuest;
+}
 
+if(personalMovieDB.count <= 10){
+  console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count <= 30 && personalMovieDB.count > 10){
+  console.log('Вы классический зритель');
+} else if(personalMovieDB.count > 30){
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
